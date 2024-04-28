269af15dc4c0f63cd686d216560ee12c7892cdb9
Yoojin93 <yoojinbae93@gmail.com>
2024-04-27 20:45:40 -0500

    adding classifiers to setup

--- src/core/setup.py
+++ src/core/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='outfit_of_the_day',
+    name='core',
     version='0.0.1',
     python_requires='>=3.10.0',
     packages=find_packages(exclude=['tests.*', 'tests']),
@@ -12,5 +12,12 @@ setup(
         'botocore',
         'dataclasses-json',
         'requests'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10'
     ]
 )
