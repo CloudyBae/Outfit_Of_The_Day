ea03b9071545fe282cb1f30a0e776a62770f602b
Yoojin93 <yoojinbae93@gmail.com>
2024-04-27 19:19:43 -0500

    testing pipeline build

--- src/handler/retrieve_weather/function.py
+++ src/handler/retrieve_weather/function.py
@@ -1,4 +1,4 @@
-def lambda_handler(event, context): #test lambda handler
+def handler(event, context): #test lambda handler
     return {
         'statusCode': 200,
         'body': 'Hello from Lambda!'
