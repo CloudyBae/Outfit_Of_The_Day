6d292c4c248d27add5effa65d390de0e82f551ae
Yoojin93 <yoojinbae93@gmail.com>
2024-04-28 16:28:27 -0500

    updating workflow and changing layer name to shared

--- src/shared/setup.py
+++ src/shared/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='core',
+    name='shared',
     version='0.0.1',
     python_requires='>=3.10.0',
     packages=find_packages(exclude=['tests.*', 'tests']),
